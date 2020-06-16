f9a14067fdd42a6515c4d465a61639be59e92af4
Bettina Johnson <johnsonbettina17@gmail.com>
2020-06-16 22:23:21 +0000

    new Direction for JS

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
 import customers from './customers.js'
+let newCustomer = document.querySelector('#name-list')
 
-let customerList = document.querySelector('#customers')
-console.log(customerList)
+console.log()
 
-let customerList = {
-name: 'Mr. Curtis Ryan';
-address: '8073 Rolling Green Rd South Valley, Missouri 99356';
-phonenumber: '(385)-917-9080';
-thumbnail: '';
-birthdate: '1967-10-29';
-since: '2007-01-12';
-}
+let newPerson = document.createElement("li")
+let personName = document.createTextNode("Mr. Curtis Ryan")
 
+let personNumber = document.createTextNode(" ")
+let personAddress = document.createTextNode("")
+let thumbNail = document.createTextNode(" ")
+let personBirthday = document.createTextNode("")
+let personSince = document.createTextNode("")
 
 
-// function 
-console.log(customerList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let customerList = document.querySelector('#customers')
+// console.log(customerList)
+
+// let customerList = {
+// name: 'Mr. Curtis Ryan';
+// address: '8073 Rolling Green Rd South Valley, Missouri 99356';
+// phonenumber: '(385)-917-9080';
+// thumbnail: '';
+// birthdate: '1967-10-29';
+// since: '2007-01-12';
+// }
+
+
+
+// // function 
+// console.log(customerList);
