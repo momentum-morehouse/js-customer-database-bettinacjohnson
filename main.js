f4e89d2b4b2bbd9d51e4fa901f1ed4db7ec262a8
Bettina Johnson <johnsonbettina17@gmail.com>
2020-06-16 22:12:29 +0000

    HTML setup and js attempt

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import customers from './customers.js'
+
+let customerList = document.querySelector('#customers')
+console.log(customerList)
+
+let customerList = {
+name: 'Mr. Curtis Ryan';
+address: '8073 Rolling Green Rd South Valley, Missouri 99356';
+phonenumber: '(385)-917-9080';
+thumbnail: '';
+birthdate: '1967-10-29';
+since: '2007-01-12';
+}
+
+
+
+// function 
+console.log(customerList);
